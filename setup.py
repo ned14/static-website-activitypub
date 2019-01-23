49e2bff3b9f17baf5d87bcc8c0c5982f8b76bacb
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-01-23 23:49:16 +0000

    More packaging fixes

--- setup.py
+++ setup.py
@@ -6,17 +6,18 @@ import os, static_website_activitypub
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Get the long description from the README file
-with open(os.path.join(here, 'README.rst')) as f:
+with open(os.path.join(here, 'Readme.rst')) as f:
     long_description = f.read()
     
 setup(
     name='static-website-activitypub',
     version=static_website_activitypub.version,
-    description='A CherryPy-based client-to-server ActivityPub service',
+    description='Wraps a static website generator with an ActivityPub client-to-server implementation',
     long_description=long_description,
     author='Niall Douglas',
     url='http://pypi.python.org/pypi/static-website-activitypub',
     packages=['static_website_activitypub'],
+    package_data={'static_website_activitypub' : ['../LICENSE']},
     test_suite='tests',
     entry_points={
         'console_scripts': [ 'static-website-activitypub=static_website_activitypub:main' ]
